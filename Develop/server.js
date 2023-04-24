ec06736fae6c4ef8c2f70cd854166bdaadfc2b54
Stephen Bondurant <stephenbondurant2014@gmail.com>
2023-04-23 20:29:58 -0500

    some progress

--- Develop/server.js
+++ Develop/server.js
@@ -1,13 +1,21 @@
 // Packages
 const express = require("express");
+const fs = require("fs");
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 const app = express();
 
 // Port
 const PORT = process.env.PORT || 3001;
 
+// Add middleware for JSON
+app.use(express.json());
+
 // GET route for homepage
 app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
@@ -16,6 +24,6 @@ app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+// POST Route to save a new note
+
+module.exports = notes;
