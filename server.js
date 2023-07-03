a49b30c6a0185258203f3b4d5bd72ee4a63a3ff1
Stephen Bondurant <stephenbondurant2014@gmail.com>
2023-07-03 09:01:49 -0500

    fixed bud

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const app = express();
-const dbNotes = require("./public/assets/js/dbnotes.js");
 
 // Port
 const PORT = process.env.PORT || 3001;
